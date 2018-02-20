f8462965bc92f5f10fb738f6054cda1798c3cfcd
ucupalandi12 <ucupers1297@gmail.com>
2018-02-20 13:00:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidstudio/datetimepickers/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.androidstudio.datetimepickers;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import static android.app.PendingIntent.getActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void	showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.date_picker));
+    }
+
+    public void	showTimePickerDialog(View view)	{
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.time_picker));
+    }
+
+    public void	processDatePickerResult(int	year, int month, int day) {
+        String month_string	= Integer.toString(month + 1);
+        String day_string =	Integer.toString(day);
+        String year_string = Integer.toString(year);
+
+        String dateMessage = (month_string + "/" + day_string +	"/"	+ year_string);
+        Toast.makeText(this, getString(R.string.date) +	dateMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    public void	processTimePickerResult(int	hourOfDay, int minute) {
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        String timeMessage = (hour_string +	":"	+ minute_string);
+        Toast.makeText(this, getString(R.string.time) +	timeMessage,Toast.LENGTH_SHORT).show();
+    }
+}
+
+
